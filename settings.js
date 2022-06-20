b6935bd560f2f25583c9e6f87ac2e38d09962862
Sasal66 <107730821+Sasal66@users.noreply.github.com>
2022-06-20 22:14:55 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,20 +19,20 @@ global.autoread = false // auto read pesan / message
 
 // Other
 global.botname = "Alphabot" //namabot kalian
-global.ownername= "hanss" //nama kalian
+global.ownername= "ᴹг defα͢͢͢ή彡" //nama kalian
 global.myweb ="https://github.com/defan312/" //bebas asal jan hapus
 global.youtube = "https://m.youtube.com/channel/UC59op3t70vIo8wiQ4BINsDw" //bebas asal jan hapus
 global.github = "https://github.com/defan312/" //bebas
 global.email = "defansugiarto5@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.ownernomer = "+6282135905896" // nomor wa kalian
-global.ownernomerr = "+62895402113558" //nmr wa kalian
+global.ownernomerr = "+682135905896" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["62895402113558","6282135905896","62895402113558"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© hanss' //sticker wm ubah
-global.author = 'Di Buat Oleh farhan' //sticker wm ganti nama kalian
+global.owner = ["6282135905896","6282135905896","6282135905896"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© ᴹг defα͢͢͢ή彡' //sticker wm ubah
+global.author = 'Di Buat Oleh defan' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
